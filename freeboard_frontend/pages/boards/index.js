87050e825633085e55acb85cdd31c0bdb2617100
yujin08 <kkkyu28@naver.com>
2024-05-27 23:08:44 +0900

    2024-05-27

---
+++ freeboard_frontend/pages/boards/index.js
@@ -0,0 +1,73 @@
+import {
+  Wrapper,
+  PostWrapper,
+  PostText,
+  InputWrapper,
+  UserWrapper,
+  Text,
+  UserInput,
+  TitleInput,
+  ContentInput,
+  ZipcodeWrapper,
+  Zipcode,
+  SearchButton,
+  ZipcodeInput,
+  ZipcodeInput2,
+  YoutubeInput,
+} from "../../styles/boards";
+
+export default function BoardsPage() {
+  return (
+    <Wrapper>
+      <PostWrapper>
+        <PostText>게시물 등록</PostText>
+      </PostWrapper>
+      <UserWrapper>
+        <InputWrapper>
+          <Text>작성자</Text>
+          <UserInput type="text" placeholder="이름을 적어주세요." />
+        </InputWrapper>
+        <InputWrapper>
+          <Text>비밀번호</Text>
+          <UserInput type="text" placeholder="비밀번호를 입력해주세요." />
+        </InputWrapper>
+      </UserWrapper>
+      <InputWrapper>
+        <Text>제목</Text>
+        <TitleInput type="text" placeholder="제목을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Text>내용</Text>
+        <ContentInput type="text" placeholder="내용을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Text>주소</Text>
+        <ZipcodeWrapper>
+          <Zipcode placeholder="07250" />
+          <SearchButton>우편번호 검색</SearchButton>
+        </ZipcodeWrapper>
+        <ZipcodeInput />
+        <ZipcodeInput2 />
+      </InputWrapper>
+      <InputWrapper>
+        <Text>유튜브</Text>
+        <YoutubeInput type="text" placeholder="링크를 복사해주세요." />
+      </InputWrapper>
+      <ImgWrapper>
+        <Text>사진 첨부</Text>
+        <ImgButtonWrapper>
+          <ImgButton>+</ImgButton>
+          <ImgButton>+</ImgButton>
+          <ImgButton>+</ImgButton>
+        </ImgButtonWrapper>
+      </ImgWrapper>
+      <OptionWrapper>
+        <Text>메인 설정</Text>
+        <RadioButton type="radio" id="youtube" name="radio-button" />
+        <RadioText htmlFor="youtube">유튜브</RadioText>
+        <RadioButton type="radio" id="image" name="radio-button" />
+        <RadioText htmlFor="image">사진</RadioText>
+      </OptionWrapper>
+    </Wrapper>
+  );
+}
---
+++ freeboard_frontend/styles/boards.js
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div``;
+
+export const PostWrapper = styled.div``;
+
+export const PostText = styled.div``;
+
+export const UserWrapper = styled.div`
+  display: flex;
+`;
+
+export const InputWrapper = styled.div`
+  //   background-color: blue;
+  margin-top: 20px;
+`;
+
+export const Text = styled.div``;
+
+export const UserInput = styled.input``;
+
+export const TitleInput = styled.input``;
+export const ContentInput = styled.input``;
+export const ZipcodeWrapper = styled.div``;
+export const Zipcode = styled.input``;
+export const SearchButton = styled.button``;
+export const ZipcodeInput = styled.input`
+  width: 100%;
+`;
+export const ZipcodeInput2 = styled.input``;
+export const YoutubeInput = styled.input``;
