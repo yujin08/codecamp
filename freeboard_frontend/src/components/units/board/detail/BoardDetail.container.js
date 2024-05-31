da393a9ae401ea20ca8d2173490252ec4d06b049
yujin08 <kkkyu28@naver.com>
2024-05-31 12:51:38 +0900

    2024-05-31

---
+++ freeboard_frontend/src/components/units/board/detail/BoardDetail.container.js
@@ -0,0 +1,13 @@
+import BoardDetailUI from "./BoardDetail.presenter";
+import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import { FETCH_BOARD } from "./BoardDetail.queries";
+
+export default function BoardDetail() {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: router.query.boardId },
+  });
+  return <BoardDetailUI data={data} />;
+}
