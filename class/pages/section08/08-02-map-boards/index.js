34bbe63be362a61c5c2fcb9bfd963db71a6d88a1
yujin08 <kkkyu28@naver.com>
2024-06-02 21:07:14 +0900

    2024-06-02

---
+++ class/pages/section08/08-02-map-boards/index.js
@@ -0,0 +1,37 @@
+import { useQuery, gql } from "@apollo/client";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARDS);
+
+  console.log(data?.fetchBoards);
+
+  //   const mystyles = {
+  //     margin: "10px",
+  //   };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>{el.number}</span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
