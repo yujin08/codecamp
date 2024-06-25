a00c3d70dd38d431e2d3aec0bf3f5b9fa19c4bdf
yujin08 <kkkyu28@naver.com>
2024-06-25 23:56:02 +0900

    2024-06-25

---
+++ class/pages/section10/10-02-typescript-boards/[number]/edit/index.tsx
@@ -0,0 +1,32 @@
+// import BoardWrite from "../../../../../BoardWrite.container";
+import BoardWrite from "../../../../src/components/units/board/10-write/BoardWrite.container";
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($number: Int) {
+    fetchBoard(number: $number) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  const router = useRouter();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { number: Number(router.query.number) },
+  });
+
+  // 한 줄일때는 괄호() 필요 없음
+  return (
+    <div>
+      <div>###########여기는 페이지입니다 ######</div>
+      <BoardWrite isEdit={true} data={data} />
+      <div>###########여기는 페이지입니다 ######</div>
+    </div>
+  );
+}
---
+++ class/src/components/units/board/10-write/BoardWrite.container.tsx
@@ -0,0 +1,87 @@
+import { useMutation } from "@apollo/client";
+import { useState, ChangeEvent } from "react";
+import { 나의그래프큐엘셋팅, updateBoard } from "./BoardWrite.queries"; // export는 골라서 가져오기 가능
+import BoardWriteUI from "./BoardWrite.presenter"; // export default로 한개만 가져오기
+import { useRouter } from "next/router";
+import { IBoardWriteProps, IMyvariables } from "./BoardWrite.types";
+// import Adkfjskldf from "./BoardWrite.presenter"; // export default로 이름 바꿔서 가져오기
+// import Adkfjskldf, { apple } from "./BoardWrite.presenter"; // export default와 export 함께 가져오기
+
+// import * as S from "./BoardWrite.presenter" // export 한방에 다 가져오기
+// S.BlueButton // export 한방에 다 가져오기
+// S.RedInput // export 한방에 다 가져오기
+
+export default function BoardWrite(props: IBoardWriteProps) {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+  const [updateBoard] = useMutation(updateBoard);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(
+      `/section10/10-02-typescript-boards/${result.data.createBoard.number}`
+    );
+  };
+
+  const onClickUpdate = async () => {
+    const myvariables: IMyvariables = {
+      number: Number(router.query.number),
+    };
+    if (writer) myvariables.writer = writer;
+    if (title) myvariables.title = title;
+    if (contents) myvariables.contents = contents;
+
+    // 여기서 수정하기 하자!!
+    const result = await updateBoard({
+      variables: {
+        number: Number(router.query.number),
+        writer: writer,
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    router.push(
+      `/section10/10-02-typescript-boards/${result.data.updateBoard.number}`
+    );
+  };
+
+  const onChangeWriter = (event: ChangeEvent<HTMLInputElement>) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event: ChangeEvent<HTMLInputElement>) => {
+    setContents(event.target.value);
+  };
+  return (
+    <div>
+      <div>$$$$$$$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$$$$$</div>
+      <BoardWriteUI
+        onClickSubmit={onClickSubmit}
+        onClickUpdate={onClickUpdate}
+        onChangeWriter={onChangeWriter}
+        onChangeTitle={onChangeTitle}
+        onChangeContents={onChangeContents}
+        isEdit={props.isEdit}
+        data={props.data} // undefined 이거나, data 이거나 둘 중 하나!
+      />
+      <div>$$$$$$$$$$$$$$ 여기는 컨테이너입니다 $$$$$$$$$$$$</div>
+    </div>
+  );
+}
