51a6be5329b01fd81af5075e75ceec9607cd03d5
yujin08 <kkkyu28@naver.com>
2024-06-26 20:33:43 +0900

    2024-06-26

---
+++ class/pages/section10/10-04-graphql-codegen-mutation/index.tsx
@@ -0,0 +1,38 @@
+import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
+import {
+  IMutation,
+  IMutationCreateBoardArgs,
+} from "../../../src/commons/types/generated/types";
+const 나의그래프큐엘셋팅 = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  // const [나의함수] = useMutation<결과타입, 변수타입>(나의그래프큐엘셋팅);
+  const [나의함수] = useMutation<
+    Pick<IMutation, "createBoard">,
+    IMutationCreateBoardArgs
+  >(나의그래프큐엘셋팅);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        // variables 이게 $ 역할을 함
+        writer: "훈이",
+        title: "안녕하세요",
+        contents: "반갑습니다",
+      },
+    });
+    console.log(result);
+  };
+
+  // 한 줄일때는 괄호() 필요 없음
+  return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>;
+}
