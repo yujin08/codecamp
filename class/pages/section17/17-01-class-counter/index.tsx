a6f0381737a2a0e8573060eeb7c691f34cd74aca
yujin08 <kkkyu28@naver.com>
2024-07-05 12:17:47 +0900

    2024-07-05

---
+++ class/pages/section17/17-01-class-counter/index.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export default class ClassCounterPage extends Component {
+  state = {
+    count: 0,
+  };
+
+  onClickCountUp = (): void => {
+    console.log(this.state.count);
+    this.setState({
+      count: 1,
+    });
+  };
+
+  render(): JSX.Element {
+    return (
+      <>
+        <div>{this.state.count}</div>
+        <button onClick={this.onClickCountUp}>카운트 올리기!!</button>
+      </>
+    );
+  }
+}
---
+++ class/pages/section17/17-02-class-lifecycle/class.ts
@@ -0,0 +1,44 @@
+// // class Date {
+// //     qqq = 3
+
+// //     getFullYear(){
+
+// //     }
+
+// //     getMonth(){
+
+// //     }
+// // }
+
+// // const data = new Date();
+// // console.log(data.getFullYear());
+// // console.log(data.getMonth());
+
+// class Monster {
+//   power = 50;
+
+//   attack() {
+//     console.log("공격합니다!!");
+//   }
+// }
+
+// // 상속
+// class 슈퍼몬스터 extends Monster {
+//   run() {
+//     console.log("도망가자!!");
+//   }
+
+//   // 오버라이딩
+//   attack() {
+//     console.log("슈퍼몬스터 필살기!!");
+//   }
+// }
+
+// const monster = new Monster();
+// monster.power; // 50
+// monster.attack(); // 공격합니다!!
+
+// const mymonster = new 슈퍼몬스터();
+// mymonster.power; // 50
+// mymonster.attack(); // 슈퍼몬스터 필살기!!!
+// mymonster.run(); // 도망가자!!
