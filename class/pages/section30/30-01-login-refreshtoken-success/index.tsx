1db781b513923dc1a43e8df85846c5364db935ae
yujin08 <kkkyu28@naver.com>
2024-08-07 14:21:57 +0900

    2024-08-07

---
+++ class/pages/section30/30-01-login-refreshtoken-success/index.tsx
@@ -0,0 +1,35 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { wrapAsync } from "../../../src/commons/libraries/asyncFunc";
+
+const FETCH_USER_LOGGED_IN = gql`
+  query {
+    fetchUserLoggedIn {
+      email
+      name
+    }
+  }
+`;
+
+export default function LoginSuccessPage(): JSX.Element {
+  // 1. 페이지 접속하면 자동으로 data에 받아지고(data는 글로벌스테이트 저장), 리렌더링됨
+  // const { data } = useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+
+  // 2. 버튼 클릭시 data에 받아지고(data는 글로벌스테이트 저장), 리렌더링됨
+  // const [나의함수, {data}] = useLazyQuery(FETCH_USER_LOGGED_IN)
+
+  // 3. axios처럼 사용하는 방법
+  // const client = useApolloClient()
+  // client.query() <==> axios.get()
+
+  const client = useApolloClient();
+
+  const onClickButton = async (): Promise<void> => {
+    const result = await client.query({
+      query: FETCH_USER_LOGGED_IN,
+    });
+    console.log(result);
+  };
+
+  return <button onClick={wrapAsync(onClickButton)}>클릭하세요</button>;
+  // return <>{data?.fetchUserLoggedIn.name}님 환영합니다!</>;
+}
---
+++ class/pages/section30/30-01-login-refreshtoken/index.tsx
@@ -0,0 +1,74 @@
+import { gql, useMutation } from "@apollo/client";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
+import type {
+  IMutation,
+  IMutationLoginUserExampleArgs,
+} from "../../../src/commons/types/generated/types";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../src/commons/stores";
+import { useRouter } from "next/router";
+import { wrapAsync } from "../../../src/commons/libraries/asyncFunc";
+
+const LOGIN_USER = gql`
+  mutation loginUserExample($email: String!, $password: String!) {
+    loginUserExample(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage(): JSX.Element {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginUserExample] = useMutation<
+    Pick<IMutation, "loginUserExample">,
+    IMutationLoginUserExampleArgs
+  >(LOGIN_USER);
+
+  const [, setAccessToken] = useRecoilState(accessTokenState);
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onClickLogin = async (): Promise<void> => {
+    try {
+      // 1. 로그인 뮤테이션 날려서 accessToken 받아오기
+      const result = await loginUserExample({
+        variables: {
+          email,
+          password,
+        },
+      });
+      const accessToken = result.data?.loginUserExample.accessToken;
+      console.log(accessToken);
+
+      // 2. 받아온 accessToken을 globalState에 저장하기
+      if (accessToken === undefined) {
+        alert("로그인에 실패했습니다! 다시 시도해 주세요!");
+        return;
+      }
+      setAccessToken(accessToken);
+
+      // 3. 로그인 성공 페이지로 이동하기
+      void router.push("/section30/30-01-login-refreshtoken-success");
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      이메일: <input type="text" onChange={onChangeEmail} />
+      비밀번호: <input type="password" onChange={onChangePassword} />
+      <button onClick={wrapAsync(onClickLogin)}>로그인</button>
+    </>
+  );
+}
