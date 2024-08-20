4960b7c7d8930a0e1be68865dc808bdfe50fee33
yujin08 <kkkyu28@naver.com>
2024-08-20 22:26:22 +0900

    2024-08-20

---
+++ class/pages/section31/31-10-data-prefetch-moved/[qqq]/index.tsx
@@ -0,0 +1,33 @@
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  const router = useRouter();
+  console.log(router);
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: router.query.qqq },
+  });
+
+  console.log(data);
+
+  return (
+    <div>
+      {/* <div>{router.query.qqq}번 게시글 이동이 완료되었습니다.</div> */}
+      <div>작성자: {data?.fetchBoard?.writer}</div>
+      <div>제목: {data?.fetchBoard?.title}</div>
+      <div>내용: {data?.fetchBoard?.contents}</div>
+    </div>
+  );
+}
---
+++ class/pages/section31/31-10-data-prefetch/index.tsx
@@ -0,0 +1,66 @@
+import { useQuery, gql, useApolloClient } from "@apollo/client";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import { MouseEvent } from "react";
+import { useRouter } from "next/router";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+    }
+  }
+`;
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage(): JSX.Element {
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  const router = useRouter();
+  const client = useApolloClient();
+
+  const prefetchBoard = (boardId: string) => async () => {
+    await client.query({
+      query: FETCH_BOARD,
+      variables: { boardId },
+    });
+  };
+
+  const onClickMove = (boardId: string) => () => {
+    void router.push(`/section31/31-10-data-prefetch-moved/${boardId}`);
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span
+            style={{ margin: "10px" }}
+            onMouseOver={prefetchBoard(el._id)}
+            onClick={onClickMove(el._id)}
+          >
+            {el.title}
+          </span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
