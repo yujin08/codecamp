98fb55c7d2d7605e31b700d6e306286861be845f
yujin08 <kkkyu28@naver.com>
2024-08-29 21:50:30 +0900

    2024-08-29

--- freeboard_frontend/src/components/units/board/detail/BoardDetail.container.js
+++ freeboard_frontend/src/components/units/board/detail/BoardDetail.container.js
@@ -1,23 +1,37 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import BoardDetailUI from "./BoardDetail.presenter";
-import { FETCH_BOARD } from './BoardDetail.queries'
+import { DELETE_BOARD, FETCH_BOARD } from "./BoardDetail.queries";
+import { FETCH_BOARDS } from "../list/BoardList.queries";
 
-export default function BoardDetail(){
-    const router = useRouter()
+export default function BoardDetail() {
+  const router = useRouter();
 
-    const { data } = useQuery(FETCH_BOARD, {
-      variables: { boardId: router.query.boardId },
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: router.query.boardId },
+  });
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  const onClickMoveToBoardEdit = () => {
+    router.push(`/boards/${router.query.boardId}/edit`);
+  };
+
+  const onClickDelete = (event) => {
+    deleteBoard({
+      variables: {
+        boardId: router.query.boardId,
+      },
+      refetchQueries: [{ query: FETCH_BOARDS }],
     });
-  
-    const onClickMoveToBoardEdit = () => {
-      router.push(`/boards/${router.query.boardId}/edit`);
-    };
-    
-    return (
-      <BoardDetailUI 
-        data={data} 
-        onClickMoveToBoardEdit={onClickMoveToBoardEdit}
-      />
-    )
+    router.push(`/boards/`);
+  };
+
+  return (
+    <BoardDetailUI
+      data={data}
+      onClickMoveToBoardEdit={onClickMoveToBoardEdit}
+      onClickDelete={onClickDelete}
+    />
+  );
 }
